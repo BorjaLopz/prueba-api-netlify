dc37211779ab42675a2ed38f2f2d7feb2a72527f
Borja <lopzdiazborja@gmail.com>
2023-10-13 18:48:04 +0200

    Primer cambio

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 const express = require("express");
-const app = express();
-
-require("dotenv").config();
+const path = require("path");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+require("dotenv").config();
 
-const port = process.env.APP_PORT || 3000;
+const PORT = process.env.PORT || 5001;
 
-app.get("/", (req, res) => {
-  res.send("Hola mundo!");
-});
-
-app.get("/prueba", (req, res) => {
-  res.send("Hola mundo desde prueba!");
-});
-
-app.listen(port, () => {
-  console.log(`Servidor escuchando en el puerto ${port}`);
-});
+express()
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("pages/index"))
+  .get("/beer", (req, res) => res.send("HOLA"))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
