e6dbd547d68a7e84518c29e3fd1e6ba6fe83e125
Borja <lopzdiazborja@gmail.com>
2023-10-13 18:57:19 +0200

    Pequeños cambios

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const path = require("path");
 
+const app = express();
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 require("dotenv").config();
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.APP_PORT || 5001;
 
 express()
   .use(express.static(path.join(__dirname, "public")))
   .set("views", path.join(__dirname, "views"))
   .set("view engine", "ejs")
-  .get("/", (req, res) => res.render("pages/index"))
+  .get("/", (req, res) => res.send("HolaMundo!"))
   .get("/beer", (req, res) => res.send("HOLA"))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
